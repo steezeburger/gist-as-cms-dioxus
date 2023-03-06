8c5aa11bc37f3707469b11c3ff154072063df2f2
Jesse Snyder <jessetsnyder@gmail.com>
2023-03-05 23:10:39 -0700

    fetch gist

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![allow(non_snake_case)]
+
+// import the prelude to get access to the `rsx!` macro and the `Scope` and `Element` types
+use dioxus::prelude::*;
+
+use crate::types::GetGistResponse;
+
+pub mod types;
+
+static GIST_API_URL: &str = "https://api.github.com/gists/";
+static GIST_ID: &str = "09c714ff93cd8afa00d00094a56bdd8e";
+
+fn main() {
+    // launch the web app
+    dioxus_web::launch(App);
+}
+
+// create a component that renders a div with the text "Hello, world!"
+fn App(cx: Scope) -> Element {
+    let get_gist = use_future(cx, (), |_| async move {
+        reqwest::get(format!("{}{}", GIST_API_URL, GIST_ID))
+            .await
+            .unwrap()
+            .json::<GetGistResponse>()
+            .await
+    });
+
+    cx.render(match get_gist.value() {
+        Some(Ok(response)) => rsx! {
+        div {
+            p {format!("{:#?}", response)}
+        }
+    },
+        Some(Err(_)) => rsx! { div { "Loading failed" } },
+        None => rsx! { div { "Loading..." } },
+    })
+}
---
+++ src/types.rs
@@ -0,0 +1,76 @@
+use std::collections::HashMap;
+
+use serde::{Serialize, Deserialize};
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct GetGistResponse {
+    pub url: String,
+    pub forks_url: String,
+    pub commits_url: String,
+    pub id: String,
+    pub node_id: String,
+    pub git_pull_url: String,
+    pub git_push_url: String,
+    pub html_url: String,
+    pub files: HashMap<String, File>,
+    pub public: bool,
+    pub created_at: String,
+    pub updated_at: String,
+    pub description: String,
+    pub comments: i64,
+    pub comments_url: String,
+    pub owner: Owner,
+    pub history: Vec<History>,
+    pub truncated: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct File {
+    pub filename: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub language: String,
+    pub raw_url: String,
+    pub size: i64,
+    pub truncated: bool,
+    pub content: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct Owner {
+    pub login: String,
+    pub id: i64,
+    pub node_id: String,
+    pub avatar_url: String,
+    pub gravatar_id: String,
+    pub url: String,
+    pub html_url: String,
+    pub followers_url: String,
+    pub following_url: String,
+    pub gists_url: String,
+    pub starred_url: String,
+    pub subscriptions_url: String,
+    pub organizations_url: String,
+    pub repos_url: String,
+    pub events_url: String,
+    pub received_events_url: String,
+    #[serde(rename = "type")]
+    pub type_field: String,
+    pub site_admin: bool,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct History {
+    pub user: Owner,
+    pub version: String,
+    pub committed_at: String,
+    pub change_status: ChangeStatus,
+    pub url: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+pub struct ChangeStatus {
+    pub total: i64,
+    pub additions: i64,
+    pub deletions: i64,
+}
